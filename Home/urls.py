ce00b8bf03923af4659339153dee6fedd28fe4c1
tushar-3549 <tushardiu3549@gmail.com>
2025-04-29 20:01:28 +0600

    building management system

---
+++ Home/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('school.urls')),
+    path('student/', include('student.urls')),
+]
