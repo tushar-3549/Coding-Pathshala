9988bccd12b9c76804422088975dd01904bc902a
tushar-3549 <tushardiu3549@gmail.com>
2025-04-30 12:28:01 +0600

    fetching std list

--- student/urls.py
+++ student/urls.py
@@ -4,7 +4,9 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.student_list, name='student_list'),
-    # path('add/', views.add_student, name='add_student')
-    path('add-student/', views.add_student, name='add-student')
+    path('add_student/', views.add_student, name='add_student'),
+    path('students/<str:slug>/', views.view_student, name='view_student'),
+    # path('student/edit/<int:pk>/', views.edit_student, name='edit_student'),
+
 
 ]
--- student/views.py
+++ student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.contrib import messages
 
@@ -66,11 +66,21 @@ def add_student(request):
         return render(request, "students/add-student.html")
 
 def student_list(request):
-    return render(request, "students/students.html")
+    student_list = Student.objects.select_related('parent').all()
+    context = {
+         'student_list': student_list
+    }
+
+    return render(request, "students/students.html", context)
 
 def edit_student(request):
     return render(request, "students/edit-student.html")
 
-def view_student(request):
-    return render(request, "students/student-details.html")
+def view_student(request, slug):
+    student = get_object_or_404(Student, student_id=slug)
+    context = {
+        'student': student
+    }
+    return render(request, "students/student-details.html", context)
+
 
