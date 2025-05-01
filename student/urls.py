c6acbb26b62f1ca918968cb5f535cf2e9ead7755
tushar-3549 <tushardiu3549@gmail.com>
2025-05-01 13:01:09 +0600

    edit and delete std data

--- student/urls.py
+++ student/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.student_list, name='student_list'),
     path('add_student/', views.add_student, name='add_student'),
-    path('students/<str:slug>/', views.view_student, name='view_student'),
-    # path('student/edit/<int:pk>/', views.edit_student, name='edit_student'),
-
+    path('view_student/<str:slug>/', views.view_student, name='view_student'),
+    path('edit_student/<str:slug>/', views.edit_student, name='edit_student'),
+    path('delete_student/<str:slug>/', views.delete_student, name='delete_student')
 
 ]
--- student/views.py
+++ student/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseForbidden
 from .models import *
 from django.contrib import messages
 
@@ -73,8 +74,57 @@ def student_list(request):
 
     return render(request, "students/students.html", context)
 
-def edit_student(request):
+def edit_student(request, slug):
+    student = get_object_or_404(Student, slug=slug)
+    parent = student.parent if hasattr(student, 'parent') else None
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        student_id = request.POST.get('student_id')
+        gender = request.POST.get('gender')
+        date_of_birth = request.POST.get('date_of_birth')
+        student_class = request.POST.get('student_class')
+        religion = request.POST.get('religion')
+        joining_date = request.POST.get('joining_date')
+        mobile_number = request.POST.get('mobile_number')
+        admission_number = request.POST.get('admission_number')
+        section = request.POST.get('section')
+        student_image = request.FILES.get('student_image')  if request.FILES.get('student_image') else student.student_image
+
+        # Retrieve parent data from the form
+        parent.father_name = request.POST.get('father_name')
+        parent.father_occupation = request.POST.get('father_occupation')
+        parent.father_mobile = request.POST.get('father_mobile')
+        parent.father_email = request.POST.get('father_email')
+        parent.mother_name = request.POST.get('mother_name')
+        parent.mother_occupation = request.POST.get('mother_occupation')
+        parent.mother_mobile = request.POST.get('mother_mobile')
+        parent.mother_email = request.POST.get('mother_email')
+        parent.present_address = request.POST.get('present_address')
+        parent.permanent_address = request.POST.get('permanent_address')
+        parent.save()
+
+        # update student information
+
+        student.first_name= first_name
+        student.last_name= last_name
+        student.student_id= student_id
+        student.gender= gender
+        student.date_of_birth= date_of_birth
+        student.student_class= student_class
+        student.religion= religion
+        student.joining_date= joining_date
+        student.mobile_number = mobile_number
+        student.admission_number = admission_number
+        student.section = section
+        student.student_image = student_image
+        student.save()
+        
+        return redirect("student_list")
+
+
     return render(request, "students/edit-student.html")
+
 
 def view_student(request, slug):
     student = get_object_or_404(Student, student_id=slug)
@@ -82,5 +132,13 @@ def view_student(request, slug):
         'student': student
     }
     return render(request, "students/student-details.html", context)
+
+def delete_student(request,slug):
+    if request.method == "POST":
+        student = get_object_or_404(Student, slug=slug)
+        student_name = f"{student.first_name} {student.last_name}"
+        student.delete()
+        return redirect('student_list')
+    return HttpResponseForbidden()
 
 
