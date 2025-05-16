c655303ea46f1744d7f3c36b93bc772d4d17fd4d
tushar-3549 <tushardiu3549@gmail.com>
2025-05-16 11:19:07 +0600

    forgot password feature added

--- Home/settings.py
+++ Home/settings.py
@@ -142,3 +142,15 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # Default backend
 )
 
+
+# Use console email backend for local development
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# If deployed
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'your-email@gmail.com'  # Replace with your email
+# EMAIL_HOST_PASSWORD = 'your-email-password'  # Replace with your email password or app-specific password
